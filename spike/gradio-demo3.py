4d653058aa8abe0e61bb5c0ece98f74feffcf956
qicaisheng <qicaisheng@qq.com>
2024-10-15 16:42:09 +0800

    feat: add chatbot

--- spike/gradio-demo3.py
+++ spike/gradio-demo3.py
@@ -13,9 +13,11 @@ client = OpenAI(
 # openai_client = OpenAI()
 
 conversation_history = []
+gradio_chatbot_histoy = []
 MAX_CONVERSATION_ROUND = 10
 MAX_CONVERSATION_HISTORY_SIZE = 2 * MAX_CONVERSATION_ROUND
 last_analyzed_image = None
+homework_description = None
 
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
@@ -76,27 +78,32 @@ def generate_html(audio_base64: str) -> str:
     """
 
 def llm_reply(message, image):
-    global conversation_history, last_analyzed_image
-    
-    if image is not None and image != last_analyzed_image:
-        homework_description = analyze_image(image)
-        system_prompt = f"""请你作为一个小学数学老师，擅长教育学生数学思维，帮助引导一步步解答数学题。过程中可以引导学生列出方程式，但是不要告诉答案。每次启发式提问不要超过2个，每次回答保持75字以内。
+    system_prompt = """请你作为一个小学数学老师，擅长教育学生数学思维，帮助引导一步步解答数学题。过程中可以引导学生列出方程式，但是不要告诉答案。每次启发式提问不要超过2个，每次回答保持75字以内。
 ## 作业描述如下：
 {homework_description}
 """
-
-        conversation_history = [{"role": "system", "content": system_prompt}]
+    global conversation_history, gradio_chatbot_histoy, last_analyzed_image, homework_description
+    
+    if image is not None and image != last_analyzed_image:
+        homework_description = analyze_image(image)
         last_analyzed_image = image
     elif not conversation_history:
         return "请先上传一张作业图片。"
-    
+
+    initial_message = {
+        "role": "system", 
+        "content": system_prompt.format(homework_description=homework_description)
+    }
+
     conversation_history = conversation_history[-MAX_CONVERSATION_HISTORY_SIZE:]
+    gradio_chatbot_histoy = gradio_chatbot_histoy[-MAX_CONVERSATION_HISTORY_SIZE:]
     conversation_history.append({"role": "user", "content": message})
+    gradio_chatbot_histoy.append([message, None])
     
     response = client.chat.completions.create(
         model="GLM-4-AirX",
         # model="gpt-4o",
-        messages=conversation_history,
+        messages=[initial_message] + conversation_history,
         stream=True
     )
     
@@ -108,6 +115,7 @@ def llm_reply(message, image):
             yield chunk.choices[0].delta.content
     
     conversation_history.append({"role": "assistant", "content": partial_message})
+    gradio_chatbot_histoy[-1][1] = partial_message
 
 
 
@@ -117,20 +125,19 @@ async def reply(text, image):
     for segment in segments:
         print(f"segment: {segment}")
         audio_file = await tts(text=segment)
-        yield generate_html(audio_file)
+        yield generate_html(audio_file), gradio_chatbot_histoy
 
 
 with gr.Blocks() as demo:
     with gr.Row():
         with gr.Column():
             img =gr.Image(type="filepath", label="上传作业图片")
-
-
-        with gr.Column():
             msg = gr.Textbox(lines=2, label="输入问题")
             submit = gr.Button("提交")
             audio_player = gr.HTML()
 
-    submit.click(reply, inputs=[msg, img], outputs=[audio_player])
+        with gr.Column():
+            chatbot = gr.Chatbot()
+    submit.click(reply, inputs=[msg, img], outputs=[audio_player, chatbot])
 
 demo.launch()
